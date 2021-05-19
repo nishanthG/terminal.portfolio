3e07c48b5539971d99e953acc10b9b9c09711e02
nishanthgunupudi <nishanth.gunupudi@oracle.com>
2021-05-19 15:29:41 +0530

    Splash page: alert

--- src/html/Splash.jsx
+++ src/html/Splash.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/splash.css";
 import {type} from '../js/splash.js'
+import warning from '../assets/warning.svg'
 
 class Splash extends React.Component {
 componentDidMount(){
@@ -19,6 +20,14 @@ componentDidMount(){
         <section className="terminal">
           <div className="terminal-text">
             <p></p>
+          </div>
+          <div className="warning">
+            <img src={warning} alt="warning" className='alert'/>
+          </div>
+          <div className="target-info">
+            <p className='person'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, ratione! Ipsa
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi quod et possimus, earum dolor optio laboriosam? Nihil adipisci voluptatem similique suscipit saepe dicta modi distinctio, doloremque in temporibus libero!
+            </p>
           </div>
         </section>
       </div>
